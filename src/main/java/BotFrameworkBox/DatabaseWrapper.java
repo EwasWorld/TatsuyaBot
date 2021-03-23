8e1cd09dac1469e95a2640bd0ae2f19e68826d88
Ewa <ella.caldeirahankey@ntlworld.com>
2021-03-23 17:39:10 +0000

    More database tests, fixed issue with checking the names of anonymous classes

--- src/main/java/BotFrameworkBox/DatabaseWrapper.java
+++ src/main/java/BotFrameworkBox/DatabaseWrapper.java
@@ -6,34 +6,71 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSerializer;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.sql.*;
 import java.util.Map;
 
 public class DatabaseWrapper {
     /**
-     * Maps the key that will be used in the database to the name of a class that extends DatabaseEntryType
-     * Used to prevent duplicate keys as keys are user-defined
+     * Maps the key that will be used in the database to a class that extends DatabaseEntryType Prevents duplicate keys
+     * as keys are user-defined
      */
-    private static final BiMap<Integer, String> databaseEntryTypes = HashBiMap.create();
+    private static final BiMap<Integer, Class> databaseEntryTypes = HashBiMap.create();
 
     /*
      * Database connection
      */
     private static final String databaseFileLocation = "Tatsuya.db";
+    private static final String sqlPrefix = "jdbc:sqlite:";
     // Used to establish the connection to the database
-    private static final String urlPrefix = "jdbc:sqlite:" + Bot.getPathToTatsuyaBot();
+    private static final String urlPrefix = sqlPrefix + Bot.getPathToTatsuyaBot();
     private static String url = urlPrefix + databaseFileLocation;
     private static final String tableName = "blobs";
     private static Connection connection = null;
 
+    /**
+     * Validates all types then stores them
+     */
     public static void setDatabaseEntryTypes(BiMap<Integer, Class> databaseEntryTypes) {
         for (Map.Entry<Integer, Class> entry : databaseEntryTypes.entrySet()) {
-            if (!DatabaseEntryType.class.isAssignableFrom(entry.getValue())) {
-                throw new BadStateException("Database entry types must extend DatabaseItem");
+            final Class clazz = entry.getValue();
+            if (clazz.isAnonymousClass()) {
+                throw new BadStateException(clazz.getName() + ": anonymous not allowed");
             }
-            DatabaseWrapper.databaseEntryTypes.put(entry.getKey(), entry.getValue().getName());
+            if (!DatabaseEntryType.class.isAssignableFrom(clazz)) {
+                throw new BadStateException(clazz.getName() + " does not extend " + DatabaseEntryType.class.getName());
+            }
+            if (!Modifier.isPublic(clazz.getModifiers())) {
+                throw new BadStateException(clazz.getName() + " must be public");
+            }
+            try {
+                // An empty constructor is required to instantiate a simple object which can then provide the
+                //      deserializer
+                // TODO Is there a way to not require an empty constructor?
+                //noinspection unchecked
+                Constructor constructor = clazz.getDeclaredConstructor();
+                if (!Modifier.isPublic(constructor.getModifiers())) {
+                    throw new BadStateException("default (no-parameter) constructor of " + clazz.getName() + " must be public");
+                }
+            }
+            catch (Exception e) {
+                throw new BadStateException(clazz.getName() + " does not have a default (no-parameter) constructor");
+            }
+            //noinspection unchecked: type is checked above
+            DatabaseEntryType instantiation = instantiate(clazz);
+            if (instantiation.getSerializer() == null) {
+                throw new BadStateException(clazz.getName() + " getSerializer returns null");
+            }
+            if (instantiation.getDeserializer() == null) {
+                throw new BadStateException(clazz.getName() + " getDeserializer returns null");
+            }
+            DatabaseWrapper.databaseEntryTypes.put(entry.getKey(), clazz);
         }
     }
 
@@ -43,20 +80,20 @@ public class DatabaseWrapper {
      *
      * @throws ContactEwaException if connection cannot be established
      */
-    private static void getConnection() {
-        if (connection == null) {
-            /*
-             * Fetches the existing database or creates a new one
-             */
-            try {
+    private static void getConnectionAndInitDb() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                /*
+                 * Fetches the existing database or creates a new one
+                 */
                 connection = DriverManager.getConnection(url);
                 if (!new File(databaseFileLocation).exists()) {
                     connection.getMetaData();
                 }
             }
-            catch (SQLException e) {
-                throw new ContactEwaException("Database connection error");
-            }
+        }
+        catch (SQLException e) {
+            throw new ContactEwaException("Database connection error");
         }
 
         /*
@@ -78,18 +115,15 @@ public class DatabaseWrapper {
         }
     }
 
-    // TODO change type param to type Class if possible
-    public static <T> T getData(String guild, DatabaseEntryType<T> type) {
-        if (!databaseEntryTypes.containsValue(type.getClass().getName())) {
-            throw new BadStateException("DatabaseEntryType not recognised: " + type.getReturnClass().getName());
-        }
-        getConnection();
+    public static <A extends DatabaseEntryType, T extends DatabaseEntryType> A getData(String guild, Class<T> type) {
+        Class<A> resolvedClass = checkAndResolveClass(type);
+        getConnectionAndInitDb();
 
         String returnedJson;
         String sql = "SELECT * FROM " + tableName + " WHERE guildId = ? AND entryType = ?";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, guild);
-            ps.setInt(2, databaseEntryTypes.inverse().get(type.getClass().getName()));
+            ps.setInt(2, databaseEntryTypes.inverse().get(resolvedClass));
             final ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 return null;
@@ -102,39 +136,81 @@ public class DatabaseWrapper {
         catch (SQLException e) {
             throw new BadStateException("Database query failed");
         }
-        return getGson(type, true).fromJson(returnedJson, type.getReturnClass());
+
+        return getGson(resolvedClass, instantiate(resolvedClass).getDeserializer()).fromJson(returnedJson, resolvedClass);
     }
 
-    public static <T> void saveData(String guild, DatabaseEntryType<T> data) {
-        if (!databaseEntryTypes.containsValue(data.getClass().getName())) {
-            throw new BadStateException("DatabaseEntryType not recognised: " + data.getReturnClass().getName());
-        }
-        getConnection();
+    /**
+     * Will overwrite the existing entry in the database for guild/dataType if it exists
+     */
+    public static <T, A extends DatabaseEntryType> void saveData(String guild, DatabaseEntryType<T> data) {
+        Class<A> resolvedClass = checkAndResolveClass(data.getClass());
+        getConnectionAndInitDb();
 
-        String json = getGson(data, false).toJson(data, data.getClass());
-        String sql = "INSERT INTO " + tableName + " (guildId, entryType, entry) VALUES(?,?,?)";
+        A typeCheckedData = resolvedClass.cast(data);
+        String json = getGson(resolvedClass, typeCheckedData.getSerializer()).toJson(typeCheckedData, resolvedClass);
+        String sql = "REPLACE INTO " + tableName + " (guildId, entryType, entry) VALUES(?,?,?)";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, guild);
-            ps.setInt(2, databaseEntryTypes.inverse().get(data.getClass().getName()));
+            ps.setInt(2, databaseEntryTypes.inverse().get(resolvedClass));
             ps.setString(3, json);
             ps.executeUpdate();
         }
         catch (SQLException e) {
-            throw new BadStateException("Database query failed");
+            throw new BadStateException("Database query failed " + e.getMessage());
         }
     }
 
-    private static Gson getGson(DatabaseEntryType type, boolean serialiser) {
-        if (serialiser) {
-            return new GsonBuilder().registerTypeAdapter(type.getReturnClass(), type.getSerializer()).create();
+    /**
+     * Ensures that the provided class is in {@link #databaseEntryTypes} Uses the immediate superclass if the provided
+     * class is anonymous
+     */
+    public static <A extends DatabaseEntryType, T extends DatabaseEntryType> Class<A> checkAndResolveClass(Class<T> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Cannot be null");
         }
-        else {
-            return new GsonBuilder().registerTypeAdapter(type.getReturnClass(), type.getDeserializer()).create();
+
+        Class returnClass = null;
+        if (databaseEntryTypes.containsValue(clazz)) {
+            returnClass = clazz;
+        }
+        else if (clazz.isAnonymousClass() && databaseEntryTypes.containsValue(clazz.getSuperclass())) {
+            returnClass = clazz.getSuperclass();
+        }
+        if (returnClass == null || !DatabaseEntryType.class.isAssignableFrom(returnClass)) {
+            throw new BadStateException(clazz.getName() + " not recognised as a database entry type, make sure it's set with setDatabaseEntryTypes");
+        }
+        //noinspection unchecked: Type is checked in above if statement
+        return returnClass;
+    }
+
+    private static <T extends DatabaseEntryType> T instantiate(Class<T> type) {
+        try {
+            return type.getDeclaredConstructor().newInstance();
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            throw new ContactEwaException("No default constructor for " + DatabaseEntryType.class.getName());
         }
     }
 
-    public static void deleteData() {
-        // TODO
+    /**
+     * @param type the type which will be converted from/to
+     * @param typeAdapter either a JsonSerializer or a JsonDeserializer
+     */
+    private static <T> Gson getGson(Class<T> type, Object typeAdapter) {
+        if (!(typeAdapter instanceof JsonSerializer || typeAdapter instanceof JsonDeserializer)) {
+            throw new BadStateException("TypeAdapter is incorrect type");
+        }
+        return new GsonBuilder().registerTypeAdapter(type, typeAdapter).create();
+    }
+
+    public static boolean deleteDatabase() {
+        if (!isInTestMode()) {
+            throw new IllegalStateException("Cannot delete database unless in test mode.");
+        }
+        closeConnection();
+        File dbFile = new File(url.substring(sqlPrefix.length()));
+        return !dbFile.exists() || dbFile.delete();
     }
 
     /**
@@ -151,15 +227,16 @@ public class DatabaseWrapper {
         return !url.equals(urlPrefix + databaseFileLocation);
     }
 
-
     /**
      * @throws ContactEwaException if an SQLException occurs
      */
     private static void closeConnection() {
+        if (connection == null) {
+            return;
+        }
         try {
-            if (connection != null) {
-                connection.close();
-            }
+            connection.close();
+            connection = null;
         }
         catch (SQLException e) {
             throw new ContactEwaException("Close connection error");
--- src/test/java/DatabaseTests.java
+++ src/test/java/DatabaseTests.java
@@ -1,9 +1,11 @@
 import BotFrameworkBox.DatabaseEntryType;
 import BotFrameworkBox.DatabaseWrapper;
 import CoreBox.DatabaseEntryHelper;
+import ExceptionsBox.BadStateException;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.gson.*;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,82 +15,268 @@ import java.util.Set;
 
 @SuppressWarnings("ConstantConditions")
 public class DatabaseTests {
+    public DatabaseTests() {
+    }
+
     public static final String guild = "GuildId";
+    private static BiMap<Integer, Class> databaseEntryTypes;
+
+    /*
+     * Interestingly, the format `new WellFormed() {{integerItem = 20}}` will result in the type being DatabaseTests$1,
+     *   since it seems to take it as an anonymous definition
+     */
+    private static final TestHelperObjects.WellFormed testData1 = new TestHelperObjects.WellFormed(
+            20,
+            "TestData",
+            new HashSet<>() {
+                {
+                    add("Item1");
+                    add("Item2");
+                }
+            }
+    );
+    public static final TestHelperObjects.WellFormed testData2 = new TestHelperObjects.WellFormed(
+            35,
+            "2 TestData 2",
+            new HashSet<>() {
+                {
+                    add("2 Item1 2");
+                    add("2 Item2 2");
+                }
+            }
+    );
 
     @BeforeEach
     public void setup() {
         DatabaseWrapper.setTestMode();
-        BiMap<Integer, Class> databaseEntryTypes = HashBiMap.create();
-        databaseEntryTypes.put(1, TestHelperObject.class);
+        databaseEntryTypes = HashBiMap.create();
+        databaseEntryTypes.put(1, TestHelperObjects.WellFormed.class);
+        // Reserved key: 99 for bad data
         DatabaseWrapper.setDatabaseEntryTypes(databaseEntryTypes);
     }
 
-    /**
-     * Simple serialize/store then retrieve/deserialize an object, ensuring it is preserved
-     */
-    @Test
-    public void simpleReadWrite() {
-        TestHelperObject item = new TestHelperObject();
-        item.integerItem = 20;
-        item.stringItem = "TestData";
-        item.setItem = new HashSet<>() {
-            {
-                add("Item1");
-                add("Item2");
-            }
-        };
-        DatabaseWrapper.saveData(guild, item);
-
-        TestHelperObject emptyItem = new TestHelperObject();
-        TestHelperObject retrievedItem = DatabaseWrapper.getData(guild, emptyItem);
-        Assertions.assertEquals(item.integerItem, retrievedItem.integerItem);
-        Assertions.assertEquals(item.stringItem, retrievedItem.stringItem);
-        Assertions.assertEquals(item.setItem, retrievedItem.setItem);
+    @AfterEach
+    public void tearDown() {
+        Assertions.assertTrue(DatabaseWrapper.deleteDatabase());
     }
 
     /**
-     * Skeleton class that can be written to and from the database
+     * Simple serialize/store then retrieve/deserialize of two objects object, ensuring it is preserved
      */
-    private static class TestHelperObject implements DatabaseEntryType<TestHelperObject> {
-        Integer integerItem = null;
-        String stringItem = null;
-        Set<String> setItem = null;
+    @Test
+    public void simpleReadWrite() {
+        String guild2 = guild + "differentGuild";
+        DatabaseWrapper.saveData(guild, testData1);
+        Assertions.assertEquals(testData1, DatabaseWrapper.getData(guild, TestHelperObjects.WellFormed.class));
+        DatabaseWrapper.saveData(guild2, testData2);
+        Assertions.assertEquals(testData1, DatabaseWrapper.getData(guild, TestHelperObjects.WellFormed.class));
+        Assertions.assertEquals(testData2, DatabaseWrapper.getData(guild2, TestHelperObjects.WellFormed.class));
+    }
 
-        public TestHelperObject() {
+    /**
+     * Ensure an anonymous instance of a DatabaseEntryType is accepted
+     */
+    @Test
+    public void anonymousClass() {
+        TestHelperObjects.WellFormed testData = new TestHelperObjects.WellFormed() {
+            {
+                integerItem = 100;
+                stringItem = "String data";
+                setItem = new HashSet<>() {
+                    {
+                        add("String1");
+                        add("String2");
+                    }
+                };
+            }
+        };
+
+        DatabaseWrapper.saveData(guild, testData);
+        Assertions.assertEquals(testData, DatabaseWrapper.getData(guild, TestHelperObjects.WellFormed.class));
+    }
+
+    /**
+     * Ensure that if the same type is written to the database from the same guild, the original data is overwritten
+     */
+    @Test
+    public void overwriteData() {
+        DatabaseWrapper.saveData(guild, testData1);
+        DatabaseWrapper.saveData(guild, testData2);
+        Assertions.assertEquals(testData2, DatabaseWrapper.getData(guild, TestHelperObjects.WellFormed.class));
+    }
+
+    /**
+     * Check that a non-public class is rejected on database wrapper setup
+     */
+    @Test
+    public void nonPublicClass() {
+        databaseEntryTypes.put(99, TestHelperObjects.PackagePrivateClass.class);
+        Assertions.assertThrows(BadStateException.class, () -> DatabaseWrapper.setDatabaseEntryTypes(databaseEntryTypes));
+    }
+
+    /**
+     * Check that a class without a default constructor is rejected on database wrapper setup
+     */
+    @Test
+    public void noDefaultConstructor() {
+        databaseEntryTypes.put(99, TestHelperObjects.NoDefaultConstructor.class);
+        Assertions.assertThrows(BadStateException.class, () -> DatabaseWrapper.setDatabaseEntryTypes(databaseEntryTypes));
+    }
+
+    /**
+     * Check that a class without a default constructor is rejected on database wrapper setup
+     */
+    @Test
+    public void privateConstructor() {
+        databaseEntryTypes.put(99, TestHelperObjects.PrivateConstructor.class);
+        Assertions.assertThrows(BadStateException.class, () -> DatabaseWrapper.setDatabaseEntryTypes(databaseEntryTypes));
+    }
+
+    /**
+     * Check that a class that doesn't implement DatabaseEntryType is rejected on database wrapper setup
+     */
+    @Test
+    public void doesNotImplement() {
+        databaseEntryTypes.put(99, TestHelperObjects.DoesNotImplement.class);
+        Assertions.assertThrows(BadStateException.class, () -> DatabaseWrapper.setDatabaseEntryTypes(databaseEntryTypes));
+    }
+
+    /**
+     * Skeleton classes that can be written to and from the database or used to trigger certain errors
+     */
+    public static class TestHelperObjects {
+        /**
+         * Required to make class instantiations and class names correct
+         * https://stackoverflow.com/questions/17006585/why-does-classname1-class-generate-in-this-situation/17006628
+         */
+        public TestHelperObjects() {
         }
 
-        @Override
-        public Class<TestHelperObject> getReturnClass() {
-            return TestHelperObject.class;
+        public static class WellFormed implements DatabaseEntryType<WellFormed> {
+            Integer integerItem = null;
+            String stringItem = null;
+            Set<String> setItem = null;
+
+            public WellFormed() {
+            }
+
+            public WellFormed(Integer integerItem, String stringItem, Set<String> setItem) {
+                this.integerItem = integerItem;
+                this.stringItem = stringItem;
+                this.setItem = setItem;
+            }
+
+            @Override
+            public Class<WellFormed> getReturnClass() {
+                return WellFormed.class;
+            }
+
+            @Override
+            public JsonDeserializer<WellFormed> getDeserializer() {
+                return (json, typeOfT, context) -> {
+                    WellFormed dti = new WellFormed();
+                    JsonObject main = json.getAsJsonObject();
+                    if (main.has("integerItem")) {
+                        dti.integerItem = main.get("integerItem").getAsInt();
+                    }
+                    if (main.has("stringItem")) {
+                        dti.stringItem = main.get("stringItem").getAsString();
+                    }
+                    if (main.has("setItem")) {
+                        dti.setItem = DatabaseEntryHelper.parseStringArray(main.get("setItem").getAsJsonArray());
+                    }
+                    return dti;
+                };
+            }
+
+            @Override
+            public JsonSerializer<WellFormed> getSerializer() {
+                return (src, typeOfSrc, context) -> {
+                    JsonObject main = new JsonObject();
+                    if (src.integerItem != null) main.addProperty("integerItem", src.integerItem);
+                    if (src.stringItem != null) main.addProperty("stringItem", src.stringItem);
+                    if (src.setItem != null) main.add("setItem", DatabaseEntryHelper.toJsonArray(src.setItem));
+                    return main;
+                };
+            }
+
+            /**
+             * {@inheritDoc}
+             * Allows objects to be compared using assertEquals too
+             */
+            @Override
+            public boolean equals(Object obj) {
+                if (!(obj instanceof WellFormed)) {
+                    return super.equals(obj);
+                }
+                WellFormed a = (WellFormed) obj;
+                return integerItem.equals(a.integerItem) && stringItem.equals(a.stringItem) && setItem.equals(a.setItem);
+            }
         }
 
-        @Override
-        public JsonDeserializer<TestHelperObject> getDeserializer() {
-            return (json, typeOfT, context) -> {
-                TestHelperObject dti = new TestHelperObject();
-                JsonObject main = json.getAsJsonObject();
-                if (main.has("integerItem")) {
-                    dti.integerItem = main.get("integerItem").getAsInt();
-                }
-                if (main.has("stringItem")) {
-                    dti.stringItem = main.get("stringItem").getAsString();
-                }
-                if (main.has("setItem")) {
-                    dti.setItem = DatabaseEntryHelper.parseStringArray(main.get("setItem").getAsJsonArray());
-                }
-                return dti;
-            };
+        static class PackagePrivateClass implements DatabaseEntryType<PackagePrivateClass> {
+            public PackagePrivateClass() {
+            }
+
+            @Override
+            public Class<PackagePrivateClass> getReturnClass() {
+                return null;
+            }
+
+            @Override
+            public JsonDeserializer<PackagePrivateClass> getDeserializer() {
+                return null;
+            }
+
+            @Override
+            public JsonSerializer<PackagePrivateClass> getSerializer() {
+                return null;
+            }
         }
 
-        @Override
-        public JsonSerializer<TestHelperObject> getSerializer() {
-            return (src, typeOfSrc, context) -> {
-                JsonObject main = new JsonObject();
-                if (src.integerItem != null) main.addProperty("integerItem", src.integerItem);
-                if (src.stringItem != null) main.addProperty("stringItem", src.stringItem);
-                if (src.setItem != null) main.add("setItem", DatabaseEntryHelper.toJsonArray(src.setItem));
-                return main;
-            };
+        public static class NoDefaultConstructor implements DatabaseEntryType<NoDefaultConstructor> {
+            NoDefaultConstructor() {
+            }
+
+            @Override
+            public Class<NoDefaultConstructor> getReturnClass() {
+                return null;
+            }
+
+            @Override
+            public JsonDeserializer<NoDefaultConstructor> getDeserializer() {
+                return null;
+            }
+
+            @Override
+            public JsonSerializer<NoDefaultConstructor> getSerializer() {
+                return null;
+            }
+        }
+
+        public static class PrivateConstructor implements DatabaseEntryType<PrivateConstructor> {
+            PrivateConstructor() {
+            }
+
+            @Override
+            public Class<PrivateConstructor> getReturnClass() {
+                return null;
+            }
+
+            @Override
+            public JsonDeserializer<PrivateConstructor> getDeserializer() {
+                return null;
+            }
+
+            @Override
+            public JsonSerializer<PrivateConstructor> getSerializer() {
+                return null;
+            }
+        }
+
+        public static class DoesNotImplement {
+            public DoesNotImplement() {
+            }
         }
     }
 }
